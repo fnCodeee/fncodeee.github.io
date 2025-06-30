c5439350899093fc6c497348b5bc34b280c10ecc
fnCodeee <faizna2213@gmail.com>
2025-06-30 22:10:35 +0700

    add new project section

---
+++ module/myproject.js
@@ -0,0 +1,7 @@
+const project_container = document.getElementById("myprojects__container");
+
+const container = document.createElement("div")
+
+container.className = 'border border-white-400 rounded';
+container.innerHTML = `
+``
