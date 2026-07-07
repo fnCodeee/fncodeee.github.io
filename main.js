4c994a16d9975d5ee38158eb3e50d85760b0c862
fnCodeee <faizna2213@gmail.com>
2026-07-07 23:59:57 +0700

    update:summary about me

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
 import LenisScrooling from "./src/libs/Lenis.js";
 const app = document.getElementById("__app");
 LenisScrooling(app);
-
-document.addEventListener("DOMContentLoaded", () => {
-  const bars = document.querySelectorAll(".bar");
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const el = entry.target;
-          el.classList.add("animate-fill");
-          observer.unobserve(el);
-        }
-      });
-    },
-    { threshold: 0.4 }
-  );
-
-  bars.forEach((bar) => observer.observe(bar));
-});
